2d322e6dcb27b2e77e6e3e92d5983a40b5476647
Edward Shao <shaoe@usc.edu>
2024-11-28 05:35:55 -0800

    Create Time.java

---
+++ modified/Time.java
@@ -0,0 +1,15 @@
+
+public class Time {
+	public Integer hour;
+	public Integer minute;
+	
+	public Time() {
+		hour = 0; 
+		minute = 0; 
+	}
+	
+	public Time(Integer hour, Integer minute) {
+		this.hour = hour; 
+		this.minute = minute; 
+	}
+}
